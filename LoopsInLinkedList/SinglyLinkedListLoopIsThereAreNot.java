d46e1e22d07866369ff144c8d3b4ad89714a0662
navaneethk86 <165456629+navaneethk86@users.noreply.github.com>
2024-08-01 15:41:15 +0530

    Update SinglyLinkedListLoopIsThereAreNot.java
    final

--- LoopsInLinkedList/SinglyLinkedListLoopIsThereAreNot.java
+++ LoopsInLinkedList/SinglyLinkedListLoopIsThereAreNot.java
@@ -65,14 +65,14 @@ public static void main(String[] args)
 {
 SinglyLinkedListLoopIsThereAreNot list = new SinglyLinkedListLoopIsThereAreNot();
 list.add(1);
-list.add(1);
-list.add(1);
-list.add(1);
-list.add(1);
-list.add(1);
-list.add(1);
-list.add(1);
-list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.add(5);
+list.add(6);
+list.add(7);
+list.add(8);
+list.add(9);
 System.out.println(list.count);
 //list.tailNode.nextNode = list.middleNodeFinder();
 System.out.println(list.loopChecker());
